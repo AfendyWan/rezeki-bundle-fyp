b8db96c4a4699a7b0e16585c0071bba07c86c5a1
AfendyWan <afendywan1999@gmail.com>
2022-03-11 12:27:39 +0800

    Create login and register functionality  without UI

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    function index(){
+        return view('dashboards.admins.index');
+     }
+     
+     function profile(){
+         return view('dashboards.admins.profile');
+     }
+ 
+     function settings(){
+            return view('dashboards.admins.settings');
+     }
+}
