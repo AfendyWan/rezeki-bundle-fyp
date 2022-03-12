4880d58a29c820cdebbbd3fcbbe16abc5328f66d
AfendyWan <afendywan1999@gmail.com>
2022-03-12 10:38:03 +0800

    Add ecommerce template for user dashboard

--- resources/views/dashboards/users/index.blade.php
+++ resources/views/dashboards/users/index.blade.php
@@ -1,25 +1,6 @@
-@extends('layouts.app')
+@extends('dashboards.users.layouts.user-dash-layout')
+@section('title', 'Dashboard')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-                <h4>Hi User: {{Auth::user()->name}}</h4>
-                <hr>
-                DASHBOARD
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+check content aaa
 @endsection
