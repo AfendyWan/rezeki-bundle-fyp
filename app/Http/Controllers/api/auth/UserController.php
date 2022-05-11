4b3ac0aaa2925a0cdd9221238849b77015e50caa
AfendyWan <afendywan1999@gmail.com>
2022-05-11 14:30:57 +0800

    Create POST API for register account

--- app/Http/Controllers/api/auth/UserController.php
+++ app/Http/Controllers/api/auth/UserController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Api\auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use App\Models\UserShippingAddress;
+use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function login(Request $request)
@@ -34,5 +37,58 @@ class UserController extends Controller
                 'errors' => "User credential is incorrect"
             ], 401);
         }
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'gender' => 'required',
+            //'birthday' => 'required',
+            'postcode' => ['required', 'numeric','digits:5'],
+            'phone_number' => 'required|numeric|regex:/(01)[0-9]/|digits_between:10,11',
+            'shipping_address' => ['required', 'string', 'max:255'],
+            'password' => ['required', 'string', 'min:8'],
+            'state' => ['required'],
+            'city' => ['required'],
+        ], [
+            'first_name.required' => 'The first name field is required.',
+            'last_name.required' => 'The last name field is required.',
+        ]);
+      
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 403);
+        }
+       
+        $newUser = new User;
+        $newUser->first_name = $request['first_name'];
+        $newUser->last_name = $request['last_name'];
+        $newUser->email = $request['email'];
+        $newUser->role = 2;
+        $newUser->gender = $request['gender'];
+        $newUser->postcode = $request['postcode'];
+        $newUser->phone_number = $request['phone_number'];
+        $newUser->shipping_address = $request['shipping_address'];
+        $newUser->password = Hash::make($request['password']);
+        $newUser->save();
+
+        $userID = $newUser->id;
+
+        UserShippingAddress::create([
+            'userID' => $userID,
+            'shipping_default_status' => 1,
+            'shipping_address' => $request['shipping_address'],
+            'state' => $request['state'],
+            'city' => $request['city'],
+            'postcode' => $request['postcode'],
+        ]);
+   
+
+        $token = $newUser->createToken('RezekiBundleCustomerAuth')->accessToken;
+
+
+        return response()->json(['token' => $token, 'name'=>$newUser->first_name ], 200);
     }
 }
